352c48cf1c3a77f3b9108ede101827c853665ade
Das <shilob.das@savethechildren.org>
2026-04-08 14:06:26 +0600

    Serialization

--- Serialization.py
+++ Serialization.py
@@ -20,13 +20,6 @@ address1 = Address(**address_dict)
 patient_dict = {'name': 'nitish', 'gender': 'male', 'age': 35, 'address': address1}
 
 patient1 = Patient(**patient_dict)
-print(patient1.address.pin)
-print(patient1.address)
-print(patient1.address.state)
-# Better organization of related data (e.g., vitals, address, insurance)
 
-# Reusability: Use Vitals in multiple models (e.g., Patient, MedicalRecord)
-
-# Readability: Easier for developers and API consumers to understand
-
-# Validation: Nested models are validated automatically—no extra work needed
+temp=patient1.model_dump(exclude={'address':['state']})
+print(temp)
